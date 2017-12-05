b977c703f1c4daac3990286a061897a6bbebdc26
meg768 <meg768@users.noreply.github.com>
2017-12-05 19:36:33 +0100

    -

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ mySwitch.prototype = {
             .setCharacteristic(Characteristic.Model, "My switch model")
             .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
 
-        var switchService = new Service.Switch(self.name);
+        var switchService = new Service.Switch("XXXX");
         switchService
             .getCharacteristic(Characteristic.On)
             .on('get', this.getSwitchOnCharacteristic.bind(this))
